e9ce41bd63e86e66da9042048fdda9fa6e61daa5
binganguk <binganguk@gmail.com>
2017-07-09 14:55:07 +0200

    chatbot and more

--- arkajagncor_chatbot.py
+++ arkajagncor_chatbot.py
@@ -9,26 +9,33 @@ Type hello to start conversation and bye to end conversation.
 chatbot_birth_date = datetime.date(year=2017, month=7, day=7)
 
 
-user_input = None
+user_input = ""
 
 
-while user_input != "bye":
-    user_input = raw_input(": ")
-    user_input = user_input.lower()
+while "bye" not in user_input:
 
+    user_input = raw_input(": ")  # Reading user input from terminal
+    user_input = user_input.lower()  # Converting the user input to lowercase
+    print user_input
+    # UPPERCASE
+    # lowercase
+    # "Hello, my name is Artur".lower() == "hello, my name is artur"
     if "name" in user_input:
         print "My name is Billbot Chattins."
 
-    elif "old" in user_input or "age" in user_input:
+    if "old" in user_input or "age" in user_input:
         print "I'm {} days old".format((datetime.date.today() - chatbot_birth_date).days)
 
-    elif "marry" in user_input:
+    if "marry" in user_input:
         print "Sorry I'm already married with a hot HP computer."
 
-    elif "hello" in user_input:
+    if "hello" in user_input:
         print "Hi, what a nice day for a talk."
 
-    
+    if "bye" in user_input:
+        print "Bye, have a nice day."
+        break
+
     else:
         print "This isn't in my vocabulary yet."
 
@@ -40,3 +47,6 @@ while user_input != "bye":
 # sss = "What is your name"
 #
 # print "name" in sss
+
+
+
