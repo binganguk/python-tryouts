04fe4b9b3a3ba3a242565b91812962d0b31dabb2
binganguk <binganguk@gmail.com>
2017-07-25 14:28:55 +0200

    working game engine

--- pokemons.py
+++ pokemons.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import random
+from time import sleep
 
 
 class Pokemon(object):
@@ -15,6 +18,7 @@ class Pokemon(object):
 
     def got_hit(self, damage):
         self.life -= damage
+        print "{} got {} damage".format(self.name, damage)
         self.status()
 
     def is_alive(self):
@@ -30,7 +34,20 @@ class Pokemon(object):
 
     def status(self):
         if not self.is_alive():
-            print "Your {} pokemon fainted".format(self.name)
+            print "\n † {} fainted\n".format(self.name)
+
+
+class Articuno(Pokemon):
+    """Articuno."""
+
+    name = "Articuno"
+    fast_attack = 101
+    fast_attack_frequency = 0.5
+    fast_attack_accuracy = 0.8
+    charged_attack = 200
+    charged_attack_frequency = 0.1
+    has_tail = True
+    life = 883
 
 
 class Bulbasaur(Pokemon):
@@ -42,36 +59,51 @@ class Bulbasaur(Pokemon):
     charged_attack = 70
     charged_attack_frequency = 0.1
     has_tail = False
-    life = 770
+    life = 1250
 
 
-class Articuno(Pokemon):
-    """Articuno."""
+class Cyndaquil(Pokemon):
+    """Cyndaquil."""
 
-    name = "Articuno"
-    fast_attack = 70
+    name = "Cyndaquil"
+    fast_attack = 200
     fast_attack_frequency = 0.5
-    charged_attack = 200
+    fast_attack_accuracy = 0.5
+    charged_attack = 70
     charged_attack_frequency = 0.1
-    has_tail = True
-    life = 500
+    has_tail = False
+    life = 680
+
 
 
 def get_attack_damage(fast_attack, fast_attack_accuracy):
-    minimal_damage = fast_attack * fast_attack_accuracy
+    minimal_damage = int(fast_attack * fast_attack_accuracy)
     return random.randint(minimal_damage, fast_attack)
 
 
 def auto_gym_battle():
     a = Articuno()
     b = Bulbasaur()
+    c = Cyndaquil()
 
-    while a.is_alive() == True and b.is_alive() == True:
+    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBattle starts\n\n\n"
+
+    counter = 1
+
+    while a.is_alive() == True and b.is_alive() == True and c.is_alive() == True:
+        print "\nRound {}\n".format(counter)
+
         a_damage = get_attack_damage(a.fast_attack, a.fast_attack_accuracy)
         b_damage = get_attack_damage(b.fast_attack, b.fast_attack_accuracy)
+        c_damage = get_attack_damage(c.fast_attack, c.fast_attack_accuracy)
 
-        a.got_hit(b_damage)
-        b.got_hit(a_damage)
+        a.got_hit(b_damage + c_damage)
+        b.got_hit(a_damage + c_damage)
+        c.got_hit(a_damage + b_damage)
+
+        counter += 1
+
+        sleep(1)
 
 
 auto_gym_battle()
