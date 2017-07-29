365a24416c9873d2303e546ba935cd7adc51b641
binganguk <binganguk@gmail.com>
2017-07-29 18:52:14 +0200

    exams

---
+++ exam1.py
@@ -0,0 +1,10 @@
+def kuku (lolo, lulu):
+    return lolo + lulu
+
+
+a = 5
+
+b = 9
+
+
+print kuku(a, b)
---
+++ exam2.py
@@ -0,0 +1,5 @@
+def moek (zynga, dadan, lugia):
+
+    return zynga * dadan *lugia
+
+print moek(3, 4, 5)
